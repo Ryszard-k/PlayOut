40649476ebb79e093d911859115aaae842d22ba7
Ryszard-k <piotr.wojcik543@gmail.com>
2021-10-09 19:19:52 +0200

    initial commit

---
+++ Web Service/src/main/java/com/inz/PlayOut/PlayOutApplication.java
@@ -0,0 +1,13 @@
+package com.inz.PlayOut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayOutApplication.class, args);
+	}
+
+}
---
+++ Web Service/src/test/java/com/inz/PlayOut/PlayOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inz.PlayOut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
